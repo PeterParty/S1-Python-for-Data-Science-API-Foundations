34c471e95c478b21c459e6670cf6c2c7a7762c18
PeterParty <petrisor.caraiman@stud.fiir.upb.ro>
2026-06-29 19:51:22 +0300

    todo list

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from models import User_input
-
+# todo: Adaug in docker fastapi si celelelalte librari
 app = FastAPI()
 
 # class User_input(BaseModel):
